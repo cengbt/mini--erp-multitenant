7e623dd3a992a39f251acffe84237d01fb2f368a
cengbt <cenglogic@gmail.com>
2026-03-23 17:22:26 +0300

    Add first products endpoint and product entity

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniErp.MultiTenant.Entities;
+
+
+namespace MiniErp.MultiTenant.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var products = new List<Product>()
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Laptop",
+                    Price = 25000m
+                }
+                ,
+                new Product
+                {
+                    Id = 2,
+                    Name = "Mouse",
+                    Price = 500m
+                }
+            };
+
+            return Ok(products);
+        }
+    }
+}
